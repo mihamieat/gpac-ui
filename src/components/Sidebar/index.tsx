bfd808bb05f184a4fdd81c5c26b259cc42e62ac9
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-09-08 12:36:29 +0200

    feat: add card component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,22 +1,25 @@
 import Link from "next/link";
+import { Card } from "@/components/Card";
 
 export default function Sidebar() {
   return (
-    <div className="h-screen w-64 bg-gray-800 text-white">
-      <nav className="flex flex-col p-4">
-        <Link className="mb-4 p-2 rounded hover:bg-gray-700" href="/overview">
-          Overview
-        </Link>
-        <Link
-          className="mb-4 p-2 rounded hover:bg-gray-700"
-          href="/notifications"
-        >
-          Notifications
-        </Link>
-        <Link className="mb-4 p-2 rounded hover:bg-gray-700" href="/settings">
-          Settings
-        </Link>
-      </nav>
+    <div className="h-screen">
+      <Card className="h-screen">
+        <nav className="flex flex-col p-4">
+          <Link className="mb-4 p-2 rounded hover:bg-gray-700" href="/overview">
+            Overview
+          </Link>
+          <Link
+            className="mb-4 p-2 rounded hover:bg-gray-700"
+            href="/notifications"
+          >
+            Notifications
+          </Link>
+          <Link className="mb-4 p-2 rounded hover:bg-gray-700" href="/settings">
+            Settings
+          </Link>
+        </nav>
+      </Card>
     </div>
   );
 }
