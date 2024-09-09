529840dd770140657a041835ec4ec07838cb7de4
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-09-09 10:29:54 +0200

    feat: add icons to navigation

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,25 +1,55 @@
 import Link from "next/link";
 import { Card } from "@/components/Card";
+import HomeIcon from "@/lib/icons/home";
+import OverviewIcon from "@/lib/icons/overview";
+import NotificationIcon from "@/lib/icons/notifications";
+import SettingsIcon from "@/lib/icons/settings";
 
 export default function Sidebar() {
   return (
     <div className="h-screen">
       <Card className="h-screen">
-        <nav className="flex flex-col p-4 ml-20">
-          <Link className="mb-4 p-2 rounded hover:bg-gray-700" href="/">
-            Home
-          </Link>
-          <Link className="mb-4 p-2 rounded hover:bg-gray-700" href="/overview">
-            Overview
+        <nav className="flex flex-col p-5 ml-20">
+          <Link className="mb-4 p-2 rounded hover:bg-violet-100" href="/">
+            <div className="flex">
+              <div className="w-5">
+                <HomeIcon />
+              </div>
+              <a>Home</a>
+            </div>
           </Link>
           <Link
-            className="mb-4 p-2 rounded hover:bg-gray-700"
+            className="mb-4 p-2 rounded hover:bg-violet-100"
+            href="/overview"
+          >
+            <div className="flex">
+              <div className="w-5">
+                <OverviewIcon />
+              </div>
+              <a>Overview</a>
+            </div>
+          </Link>
+          <Link
+            className="mb-4 p-2 rounded hover:bg-violet-100"
             href="/notifications"
           >
-            Notifications
+            <div className="flex">
+              <div className="w-5">
+                <NotificationIcon />
+              </div>
+              <a>Notifications</a>
+            </div>
           </Link>
-          <Link className="mb-4 p-2 rounded hover:bg-gray-700" href="/settings">
-            Settings
+          <Link
+            className="mb-4 p-2 rounded hover:bg-violet-100"
+            href="/settings"
+          >
+            <div className="flex">
+              <div className="w-5">
+                <SettingsIcon />
+              </div>
+              <a>Settings</a>
+            </div>
           </Link>
         </nav>
       </Card>
