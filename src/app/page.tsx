e39bc9a39e5a56b08303bc29458817d7800eb13d
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-09-12 20:25:55 +0200

    fix: correct disapearing home illustration when in dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,18 +11,14 @@ export default function Home() {
           By @mihamieat & @eteph
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/illustration.svg"
-          alt="Machine learnign illustration"
-          width={500}
-          height={500}
-          priority
-        />
-      </div>
-
+      <Image
+        className="relative"
+        src="/illustration.svg"
+        alt="Machine learnign illustration"
+        width={500}
+        height={500}
+        priority
+      />
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://github.com/mihamieat/gpac-api" // TODO: change to actual API's swagger doc url
